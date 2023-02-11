e931f938c2e8e2d9945273561481686c057752b0
pmikola <pio.mikolajczyk@gmail.com>
2023-02-11 16:51:29 +0100

    grating step calc

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import math as m
 
 import numpy as np
+import sympy
 from sympy.solvers import solve
 from sympy import Symbol
 from scipy.constants import pi
 from numpy import linspace, arange, tan
 from numpy.lib.scimath import sqrt
 import matplotlib.pyplot as plt
-
-
 
 
 class GCP:
@@ -23,9 +22,10 @@ class GCP:
         self.k_n_core = GCP.complexWavenumver(self.k_0, self.n_core)
         self.k_n_substrate = GCP.complexWavenumver(self.k_0, self.n_substrate)
         self.core_h = core_height
-        GCP.coreSlabTEMmodes(self, 100,1)
+        GCP.coreSlabTEMmodes(self, 1000, 1)
         # TODO : Add effective index calculation for width and height dimention of waveguide
-    def coreSlabTEMmodes(self, no_points,plot_slab):
+
+    def coreSlabTEMmodes(self, no_points, plot_slab):
         # beta**2 + kappa**2 = k**2 => Pythagorean formula for variables
         # rule of tumb => max(n_substrate, n_cladding) <= neff <= n_core
         self.kappamax = sqrt((self.k_n_core * self.n_core) ** 2 - (self.k_n_substrate * self.n_substrate) ** 2)
@@ -35,14 +35,21 @@ class GCP:
         self.gamma_substrate = sqrt(self.beta ** 2 - (self.k_n_substrate * self.n_substrate) ** 2)
         self.gamma_cladding = sqrt(self.beta ** 2 - (self.k_n_cladding * self.n_cladding) ** 2)
         self.y1 = tan(self.core_h * self.kappa)
-        self.y2 = (self.gamma_cladding + self.gamma_substrate) / (self.kappa * (1 - (self.gamma_cladding * self.gamma_substrate / self.kappa ** 2)))
-        self.y3 = self.kappa*((self.n_core**2/self.n_substrate**2) * self.gamma_substrate + (self.n_core**2/self.n_cladding**2)*self.gamma_cladding) / (self.kappa**2 - (self.n_core**4 / ((self.n_cladding ** 2) * (self.n_substrate**2))*self.gamma_cladding * self.gamma_substrate))
+        self.y2 = (self.gamma_cladding + self.gamma_substrate) / (
+                    self.kappa * (1 - (self.gamma_cladding * self.gamma_substrate / self.kappa ** 2)))
+        self.y3 = self.kappa * ((self.n_core ** 2 / self.n_substrate ** 2) * self.gamma_substrate + (
+                    self.n_core ** 2 / self.n_cladding ** 2) * self.gamma_cladding) / (self.kappa ** 2 - (
+                    self.n_core ** 4 / (
+                        (self.n_cladding ** 2) * (self.n_substrate ** 2)) * self.gamma_cladding * self.gamma_substrate))
+        self.n_eff = self.beta / self.k_n_core
+        self.avg_n_eff = np.average(np.array(self.n_eff))
         # TODO Search for crossing functions
-        #
+        # TODO What is effective index for this waveguide per mode?
+        # beta / k = neff (znaleźć index w becie odpowiadający przecięciu kappy dla modu) :)
+
         # self.y_substracted = np.array(abs(abs(self.y1) - abs(self.y2)))
         # idx = np.argpartition(self.y_substracted, 3)
         # print(idx,self.y_substracted[idx[:3]],self.kappa[idx[:3]])
-
 
         if plot_slab == 1:
             plt.figure()
@@ -51,14 +58,16 @@ class GCP:
             plt.xlabel('kappa')  # x axis label
             plt.ylabel('Transcendental Funcs')
             plt.axis([0, self.kappamax, -10, 10])
-            plt.plot(self.kappa, self.y1,label='tanh()')
-            plt.plot(self.kappa, self.y2,label='TEmodes')
-            plt.plot(self.kappa, self.y3,label='TMmodes')
+            plt.plot(self.kappa, self.y1, label='tanh()')
+            plt.plot(self.kappa, self.y2, label='TEmodes')
+            plt.plot(self.kappa, self.y3, label='TMmodes')
+            plt.plot(self.kappa, self.n_eff, label='n_eff')
             plt.legend()
             plt.show()
+
+
         else:
             pass
-
 
     @staticmethod
     def complexWavenumver(k_0, n):
@@ -79,18 +88,21 @@ class GCP:
     def BraggPhaseMatch(m, k_0, n_cladding, theta, n_eff):
         # Bragg Phase Match condition
         grating_period = Symbol('grating_period')
-        BPM_eqn = ((k_0 * n_cladding * m.sin(theta) + m * 2 * m.pi / grating_period) / k_0) - n_eff
+        BPM_eqn = ((k_0 * n_cladding * sympy.sin(theta) + m * 2 * sympy.pi / grating_period) / k_0) - n_eff
         solution = solve(BPM_eqn, grating_period)
         return solution
 
 
-wl = 1.683 # wavelength
-n_clad = 1.7
-n_c = 3.5
-n_sub = 1.4
-c_h = 0.1E-6
+# SiN + SiO2
+wl = 1.683  # wavelength
+n_clad = 1.44
+n_c = 2.45
+n_sub = 1.44
+c_h = 0.45E-6
 
 G = GCP(wl, n_clad, n_c, n_sub, c_h)
 
 
+grating_step = G.BraggPhaseMatch(1,G.k_0,G.n_cladding,10,G.avg_n_eff)
 
+print(grating_step)
